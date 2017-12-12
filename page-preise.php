34fb909abd5030814de619a792d05f51ed1e7d38
nep0muck <me@eichlersolutions.de>
2017-12-12 15:49:24 +0100

    Added acf and custom price page

---
+++ page-preise.php
@@ -0,0 +1,93 @@
+<?php get_header(); ?>
+
+  <main role="main">
+    <!-- section -->
+    <section>
+
+      <h1><?php the_title(); ?></h1>
+
+    <?php if (have_posts()): while (have_posts()) : the_post(); ?>
+
+      <!-- article -->
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+        <?php the_content(); ?>
+
+        <?php comments_template( '', true ); // Remove if you don't want comments ?>
+
+        <?php
+
+        // check if the flexible content field has rows of data
+        if( have_rows('preisliste') ):
+
+          // loop through the rows of data
+          while ( have_rows('preisliste') ) : the_row();
+
+            // check current row layout
+              if( get_row_layout() == 'leistungen' ):
+
+                $behandlungskategorie = get_sub_field('behandlungskategorie');
+                $behandlungskategorie_beschreibung = get_sub_field('behandlungskategorie_beschreibung');
+
+                echo '<h2>' . $behandlungskategorie . '</h2>';
+                echo '<p>' . $behandlungskategorie_beschreibung . '</p>';
+
+                // check if the nested repeater field has rows of data
+                if( have_rows('behandlungen') ):
+
+                  echo '<table>';
+
+                  // loop through the rows of data
+                    while ( have_rows('behandlungen') ) : the_row();
+
+                    $behandlung = get_sub_field('behandlung');
+                    $beschreibung = get_sub_field('beschreibung');
+                    $preis = get_sub_field('preis');
+
+                    echo '<tr>';
+                      echo '<td>' . $behandlung . '</td>';
+                      echo '<td>' . $beschreibung . '</td>';
+                      echo '<td>' . $preis . '</td>';
+                    echo '</tr>';
+
+                  endwhile;
+
+                  echo '</table>';
+                endif;
+              endif;
+          endwhile;
+
+        else :
+
+            // no layouts found
+
+        endif;
+
+        ?>
+
+        <br class="clear">
+
+        <?php edit_post_link(); ?>
+
+      </article>
+      <!-- /article -->
+
+    <?php endwhile; ?>
+
+    <?php else: ?>
+
+      <!-- article -->
+      <article>
+
+        <h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+
+      </article>
+      <!-- /article -->
+
+    <?php endif; ?>
+
+    </section>
+    <!-- /section -->
+  </main>
+
+<?php get_footer(); ?>
